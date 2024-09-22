ec6440741d8b0e6f658ef659b8d478a13105f910
Mohammed Yassine Bakhtaoui <mohammedyassine808@gmail.com>
2024-09-22 21:51:00 +0100

    5000

--- server.js
+++ server.js
@@ -529,7 +529,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(10000, () => console.log("Server listening on port 10000 ..."));
+    app.listen(5000, () => console.log("Server listening on port 5000 ..."));
   })
   .catch((err) => {
     console.log(err);
